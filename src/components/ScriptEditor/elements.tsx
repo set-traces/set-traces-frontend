73cd4ee61da4581b12cc8c30fc447bef3f4ff425
Eirik Skjærseth <eirik1502@gmail.com>
2020-04-01 17:22:58 +0200

    add simple text editing with draftjs in ScriptEditor component. Quill code moved to ScriptEditorQuill component for now

---
+++ src/components/ScriptEditor/elements.tsx
@@ -0,0 +1,78 @@
+import styled from "styled-components"
+import React from "react"
+import { Role as RoleType, Script } from "../../api/dataTypes"
+
+export const Wrapper = styled.div`
+  //width: 100%;
+`
+
+export const BackgroundPaper = styled.div`
+  background-color: #fff;
+  box-shadow: 0px 0px 1px 1px rgba(214, 214, 214, 1);
+  //padding: 0 10% 100px 10%;
+  width: ${800 - 96 * 2}px;
+  //min-width: 400px;
+  //width: 100%;
+  padding: 96px;
+  box-sizing: border-box;
+  //display: flex;
+  //flex-direction: column;
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  //font-size: 14px;
+`
+
+export const Title = styled.h1`
+  color: ${(props) => props.theme.colors.undertone};
+  margin-top: 0;
+  margin-bottom: 32px;
+`
+
+export const Description = styled.span`
+  margin-bottom: 32px;
+`
+
+export const RolesMetaContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding-bottom: 24px;
+  margin-bottom: 48px;
+  border-bottom: 1px solid ${(props) => props.theme.colors.undertone};
+`
+export const Role = styled.span<{ color?: string }>`
+  //border-bottom: 2px solid ${(props) => props.color};
+  background-color: ${(props) => props.color || "#8888ff"};
+  padding: 2px;
+  border-radius: 3px;
+`
+
+const RolesMetaRole = styled(Role)`
+  margin: 5px;
+`
+
+type ScriptHeaderProps = {
+  script: Script
+  rolesColors: Record<RoleType, string>
+}
+
+export const ScriptHeader: React.FC<ScriptHeaderProps> = ({ script, rolesColors }) => {
+  return (
+    <span>
+      <Title>{script.name}</Title>
+      <RolesMetaRole color={"#00000000"}>
+        <strong>Context:</strong>
+      </RolesMetaRole>
+      <Description>{script.description}</Description>
+      <RolesMetaContainer>
+        <RolesMetaRole color={"#00000000"}>
+          <strong>Roles: </strong>
+        </RolesMetaRole>
+        {script.rolesMeta.map((roleMeta, i) => (
+          <RolesMetaRole key={i} color={rolesColors[roleMeta.role]}>
+            {roleMeta.role}
+          </RolesMetaRole>
+        ))}
+      </RolesMetaContainer>
+    </span>
+  )
+}
--- src/components/ScriptEditor/index.tsx
+++ src/components/ScriptEditor/index.tsx
@@ -1,40 +1,22 @@
-import React, { ReactElement, useCallback, useEffect, useRef, useState } from "react"
-import { RoleMeta, Script, ScriptLine, ScriptLineType } from "../../api/dataTypes"
+import React, { useState } from "react"
+import { Role as RoleType, Script, ScriptLineType } from "../../api/dataTypes"
+import { BackgroundPaper, Wrapper, ScriptHeader, Role } from "./elements"
+import Draft, {
+  CompositeDecorator,
+  ContentState,
+  DraftDecorator,
+  Editor,
+  EditorState,
+  ContentBlock,
+  genKey,
+} from "draft-js"
 import styled from "styled-components"
 import { theme } from "../../Theme"
-import ReactQuill, { Quill } from "react-quill"
-import { UnprivilegedEditor } from "./ReactQuillTypesRedeclare"
-import * as QuillTypes from "quill"
-
-import "react-quill/dist/quill.core.css"
-import "./customQuillStyles.css"
-import { registerCustomBlots } from "./customBlots"
-import useDebounce from "../../hooks/timing"
 
 type Props = {
-  className?: any
+  className?: string
   script: Script
 }
-
-const Wrapper = styled.div`
-  //margin-bottom: 100px;
-  width: 100%;
-`
-
-const BackgroundPaper = styled.div`
-  background-color: #fff;
-  box-shadow: 0px 0px 1px 1px rgba(214, 214, 214, 1);
-  //padding: 0 10% 100px 10%;
-  //width: ${800 - 96 * 2}px;
-  //min-width: 400px;
-  width: 100%;
-  padding: 96px;
-  box-sizing: border-box;
-  //display: flex;
-  //flex-direction: column;
-  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-  //font-size: 14px;
-`
 
 const COLOR_ALPHA = "aa"
 const ROLES_COLORS = [
@@ -45,144 +27,71 @@ const ROLES_COLORS = [
   theme.colors.highlight + COLOR_ALPHA,
 ]
 
-const Delta = Quill.import("delta")
+const roleDecorator: DraftDecorator = {
+  strategy: (contentBlock, callback, contentState) => {
+    const text = contentBlock.getText()
 
-const lineActionPrefix = "*"
-
-const mapEditorContentsToLines = (linesText: string[], rolesMeta: RoleMeta[]): ScriptLine[] => {
-  // console.log("Line deltas:")
-  // delta.eachLine((lineDelta: QuillTypes.Delta, attributes) => {
-  //   console.log(lineDelta)
-  //   // assuming delete deltas will not be present
-  // })
-  const roles = rolesMeta.map((roleMeta) => roleMeta.role)
-
-  return linesText.map((lineText) => {
-    if (lineText.startsWith(lineActionPrefix)) {
-      return {
-        type: ScriptLineType.ACTION,
-        roles: [],
-        text: lineText.slice(lineActionPrefix.length),
-      }
+    if (text.includes(":")) {
+      callback(0, text.indexOf(":"))
     }
-    // check if a role is present
-    if (lineText.includes(": ")) {
-      const [lineRole, restText] = lineText.split(": ", 2)
-      if (roles.includes(lineRole)) {
-        return {
-          type: ScriptLineType.REMARK,
-          role: lineRole,
-          text: restText,
-        }
-      }
-    }
-
-    return {
-      type: ScriptLineType.COMMENT,
-      text: lineText,
-    }
-  })
+  },
+  component: Role,
 }
 
-const mapScriptLinesToEditorContent = (scriptLines: ScriptLine[]): QuillTypes.Delta => {
-  const lineDeltas: QuillTypes.Delta[] = scriptLines.map((scriptLine: ScriptLine) => {
-    switch (scriptLine.type) {
-      case ScriptLineType.REMARK:
-        return new Delta()
-          .insert(scriptLine.role, { role: true })
-          .insert(": ")
-          .insert(scriptLine.text)
-          .insert("\n")
-      case ScriptLineType.ACTION:
-        return new Delta().insert(lineActionPrefix).insert(scriptLine.text).insert("\n")
-      case ScriptLineType.COMMENT:
-        return new Delta().insert(scriptLine.text, { comment: true }).insert("\n")
-      default:
-        return new Delta().insert("\n")
-    }
-  })
-  return lineDeltas.reduce((acc, curr) => curr.compose(acc), new Delta())
-}
+const compositDecorator = new CompositeDecorator([roleDecorator])
 
-registerCustomBlots()
+let initialEditorState = EditorState.createEmpty(compositDecorator)
+const contentState = initialEditorState.getCurrentContent()
 
-// const quillModules =
-const defaultValue = new Delta([])
-const formats = [
-  "role",
-  "comment",
-  "header",
-  "bold",
-  "italic",
-  "underline",
-  "link",
-  "strike",
-  "clean",
-]
-
-const ScriptEditor: React.FC<Props> = (props) => {
-  const { className, script: initialScript } = props
-
-  const modules = useRef({
-    toolbar: [],
-  })
-
-  const [script, setScript] = useState<Script>(initialScript)
-
-  const [editorContent, setEditorContent] = useState<QuillTypes.Delta>(
-    mapScriptLinesToEditorContent(initialScript.lines),
+const ScriptEditor: React.FC<Props> = ({ className, script }) => {
+  const rolesColors: Record<RoleType, string> = {}
+  script.rolesMeta.forEach(
+    (roleMeta, i) => (rolesColors[roleMeta.role] = ROLES_COLORS[i % ROLES_COLORS.length]),
   )
 
-  const editorRef = useRef<ReactQuill | null>(null)
-
-  useEffect(() => {
-    if (script) {
-      const delta = mapScriptLinesToEditorContent(script.lines)
-      setEditorContent(delta)
-    }
-  }, [script])
-
-  const handleQuillChange = useCallback(
-    (
-      content: string,
-      changeDelta: QuillTypes.Delta,
-      source: QuillTypes.Sources,
-      editor: UnprivilegedEditor,
-    ) => {
-      console.log("source", source)
-      // source user means that the content was changed by the user
-      // should not map the content to our format if the content was changed as a response to our format change
-      if (source === "user") {
-        const selection = editor.getSelection()
-        // console.log("selection", selection)
-        // console.log("content", content)
-        const allLinesText = editor.getText().split("\n")
-        const scriptLines = mapEditorContentsToLines(allLinesText, script.rolesMeta)
-
-        // setTimeout(() => setScript({ ...script, lines: scriptLines }), 5)
-        setScript({ ...script, lines: scriptLines })
+  const scriptContentBlockArray: ContentBlock[] = script.lines
+    .map((line): ContentBlock[] => {
+      switch (line.type) {
+        case ScriptLineType.REMARK:
+          return [
+            new ContentBlock({
+              key: genKey(),
+              type: "unstyled",
+              text: line.role + ": ",
+              data: { color: rolesColors[line.role] },
+            }),
+            new ContentBlock({
+              key: genKey(),
+              type: "unstyled",
+              text: line.text,
+            }),
+          ]
+        default:
+          return [new ContentBlock({ key: genKey(), type: "unstyled", text: line.text })]
       }
-    },
-    [script],
+    })
+    .flatMap((contentBlockArray) => contentBlockArray)
+
+  // .contentState.createEntity("role", "mutable", { color })
+
+  const [editorState, setEditorState] = useState<EditorState>(
+    EditorState.push(
+      initialEditorState,
+      ContentState.createFromBlockArray(new Array<ContentBlock>(...scriptContentBlockArray)),
+      "insert-characters",
+    ),
   )
 
-  console.log("editor content", editorContent)
+  const handleEditorChange = (editorState: EditorState) => {
+    setEditorState(editorState)
+  }
 
   return (
     <Wrapper className={className}>
-      <BackgroundPaper key={script.id + "1"} id={script.id + "1"}>
-        <ReactQuill
-          ref={editorRef}
-          key={script.id}
-          id={script.id}
-          // workaround as using the value prop gives error
-          // defaultValue={editorContent}
-          value={editorContent}
-          onChange={handleQuillChange}
-          modules={modules.current}
-          formats={formats}
-          theme={undefined}
-        />
+      <BackgroundPaper>
+        <ScriptHeader script={script} rolesColors={rolesColors} />
+
+        <Editor editorState={editorState} onChange={handleEditorChange} />
       </BackgroundPaper>
     </Wrapper>
   )
